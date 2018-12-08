3e586be57f193aec430ce42b595d660155da5218
Evan Zoran <evanzoran@Evans-MacBook-Air.local>
2018-12-08 13:28:05 -0700

    added grid

---
+++ Assets/Scripts/Grid/HomeGrid.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeGrid : MonoBehaviour {
+
+	public GameObject space;
+	public List<GameObject> grid;
+	public int n; //grid size
+ 
+	// Use this for initialization
+	void Start () {
+		n = 3;
+		grid = new List<GameObject> ();
+
+		CreateGrid();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		CheckGrid ();
+	}
+
+	void CreateGrid()
+	{
+		//place grids down;
+
+		for (int i = 0; i < (n*n); i++) 
+		{
+			GameObject plant_space;
+			plant_space = Instantiate (space);
+			grid.Add(plant_space);
+		}
+
+	}
+
+	void PlaceInGrid()
+	{
+		
+	}
+
+	void CheckGrid()
+	{
+		int tally = 0;
+		for (int i = 0; i < grid.Count; i++) 
+		{
+			if (grid[i].GetComponent<PlantSpace>().getIsOccupied()) 
+			{
+				tally++;
+			}
+		}
+
+		Debug.Log (tally);
+	}
+}
---
+++ Assets/Scripts/Grid/PlantSpace.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantSpace : MonoBehaviour {
+
+	public bool isOccupied;
+	public static Vector2 spacePosition;
+
+	// Use this for initialization
+	void Start () {
+		spacePosition = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool getIsOccupied()
+	{
+		return isOccupied;
+	}
+
+	public void setIsOccupied(bool b)
+	{
+		isOccupied = b;
+	}
+}
