928f697a24707d6b39b5dbe509a95aed0bb0be04
ezoran <evanzoran17@gmail.com>
2018-12-09 11:16:10 -0700

    added clock face scripts and png

---
+++ Assets/ClockFaceChange.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockFaceChange : MonoBehaviour {
+	private Clock _clock;
+
+	public Sprite day;
+	public Sprite night;
+
+	private void OnEnable() {
+
+		Clock _clock = Utils.GetClock();
+		Debug.Log (_clock);
+		if (_clock) {
+			_clock.Tick += ClockTick;
+		}
+	}
+
+	private void OnDisable() {
+		if (_clock) {
+			_clock.Tick -= ClockTick;
+		}
+	}
+
+	void Start()
+	{
+		Clock _clock = Utils.GetClock ();
+		gameObject.GetComponent<SpriteRenderer>().sprite = day;
+			
+
+	}
+	public void ClockTick(float timeOfDay) {
+
+		Debug.Log ("Move Hand");		
+		Debug.Log (_clock);
+
+		ChangeFace (timeOfDay);
+	}	
+
+	void ChangeFace(float timeOfDay)
+	{
+		if (timeOfDay == 12) {
+			gameObject.GetComponent<SpriteRenderer> ().sprite = night;
+		} else if (timeOfDay == 0) {
+			gameObject.GetComponent<SpriteRenderer>().sprite = day;
+
+		}
+	}
+
+}
---
+++ Assets/ClockHandMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockHandMovement : MonoBehaviour {
+
+	private Clock _clock;
+	float ratio;
+
+	private void OnEnable() {
+		
+		Clock _clock = Utils.GetClock();
+		ratio = _clock.settings.hoursPerSecond * _clock.settings.tickTime;
+		Debug.Log (_clock);
+		if (_clock) {
+			_clock.Tick += ClockTick;
+		}
+	}
+
+	private void OnDisable() {
+		if (_clock) {
+			_clock.Tick -= ClockTick;
+		}
+	}
+
+	void Start()
+	{
+		Clock _clock = Utils.GetClock ();
+
+	}
+	public void ClockTick(float timeOfDay) {
+
+		Debug.Log ("Move Hand");		
+		Debug.Log (_clock);
+	
+		MoveHand (timeOfDay);
+	}	
+
+	void MoveHand(float timeOfDay)
+	{
+		Vector3 z_axis = new Vector3(0,0,1); //z-axis
+
+		float r = 3 / ratio; 
+		float angle = 90 / r;
+
+		gameObject.transform.RotateAround (gameObject.transform.position, z_axis, -angle);
+	}
+
+
+}
